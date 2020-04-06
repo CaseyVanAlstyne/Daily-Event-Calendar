e3f224b21cb730de0c96241b1751be0261f9bb78
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-04-06 10:24:06 -0400

    set up running clock using moment and a setinterval timer. Also set up a way to capture all time slot elements on page, which will be used to compare the current time to create a dynamic response for the past present and future settings in CSS.

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-var currentDay = document.getElementsById("currentDay");
-var userInput = document.getElementById("user-input");
+$(document).ready(function(){
+   
+var currentDay = $("#currentDay");
+var userInput = $("#user-input");
+var time = moment().format('ha');
+// var m = moment().format('MMMM Do YYYY');
+var allTimeSlots = $(".content");
+    console.log(allTimeSlots);
 
-$(window).load(function(currentDay){
-    moment().format('MMMM Do YYYY, h:mm:ss a');
-    console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+// display the current date and time in the currentDay Id on the HTML document
+function updateTime() {
+currentDay.text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+    console.log(time);
+}
+setInterval(updateTime, 0);
+
+// use for loop to compare time (ha) to the id for the time, which is foun on the HTML document. 
+for (let i = 0; i < allTimeSlots.length; i++) {
+    console.log(allTimeSlots[i])
+    // compare time with Ids
+    // if (time !== currentDay) {
+    //     allTimeSlots.style.color = "#99badd";
+}
+
+// function updateContent() {
+//     "9am".innerHTML = localStorage.getItem("nine");
+//     "10am".innerHTML = localStorage.getItem("ten");
+//     "11am".innerHTML = localStorage.getItem("eleven");
+//     "12pm".innerHTML = localStorage.getItem("twelve");
+//     "1pm".innerHTML = localStorage.getItem("one");
+//     "2pm".innerHTML = localStorage.getItem("two");
+//     "3pm".innerHTML = localStorage.getItem("three");
+//     "4pm".innerHTML = localStorage.getItem("four");
+//     "5pm".innerHTML = localStorage.getItem("five");
+//     }
+
 });
