8a77cfc6ad3f7cf5646b598768ba91884dc95251
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-04-08 21:56:18 -0400

    trying new logic to use less than and greater than operators for the comparison of time.

--- script.js
+++ script.js
@@ -3,47 +3,71 @@ $(document).ready(function () {
     var currentDay = $("#currentDay");
     var userInput = $("#user-input");
     var time = moment().format('H');
-    // var m = moment().format('MMMM Do YYYY');
     var allTimeSlots = $(".content");
     console.log(allTimeSlots);
 
-    // display the current date and time in the currentDay Id on the HTML document
+    // display the current date and time in the currentDay Id on the HTML document------------------------------------------------------------------
     function updateTime() {
         currentDay.text(moment().format('dddd, MMMM Do YYYY, H'))
         console.log(time);
     }
     setInterval(updateTime, 1000);
 
-    // use for loop to compare time (ha) to the id for the time, which is foun on the HTML document. 
-    for (let i = 0; i < allTimeSlots.length; i++) {
-        console.log(allTimeSlots[i])
-        console.log(time);
-        console.log(currentDay);
-        // add class to each of the textarea elements for allTimeSlots
-        if (time !== currentDay) {
-            $(allTimeSlots[i]).addClass("past");
-        } else if (time === currentDay) {
-            $(allTimeSlots[i]).addClass("present");
-        } else {
-            $(allTimeSlots[i]).addClass("future");
-        }
-        // do I need to use less than and/or greater than for the past/future?
-    }
+    // use for loop to compare time (H) to currentDay-----------------------------------------------------------------------------------------------
 
-    // start localstorage here---Need a setItem
+    // for (let i = 0; i < allTimeSlots.length; i++) {
+    //     // console.log(allTimeSlots[i])
+    //     // console.log(time);
+    //     // console.log(currentDay);
+
+    //     // add class to each of the textarea elements for allTimeSlots------------------------------------------------------------------------------
+    //     if (time >= 8 && time < 9) {
+    //         $(allTimeSlots[i]).addClass("present");
+    //     }
+
+    //     if (time !== 8) {
+    //         $(allTimeSlots[i]).addClass("past");
+    //     } else if (time === currentDay) {
+    //         $(allTimeSlots[i]).addClass("present");
+    //     } else {
+    //         $(allTimeSlots[i]).addClass("future");
+    //     }
+    // do I need to use less than and / or greater than for the past / future ?
+    // }
+
+    allTimeSlots.each(function () {
+        console.log($(this).attr("id"));
+        var loopId = $(this).attr("id")
+        var parsedLoop = parseInt(loopID);
+        console.log(typeof parsedLoop);
+        console.log(typeof loopId);
+        console.log(typeof time);
+        if (time < loopId) {
+            $(this).addClass("past");
+        } else if (time > loopId) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    });
+
+
+
+    // start localstorage here---Need a setItem---------------------------------------------------------------------------------------------------------
 
     // function updateContent() {
+    //     "8".innerHTML = localStorage.getItem("eight");
     //     "9".innerHTML = localStorage.getItem("nine");
     //     "10".innerHTML = localStorage.getItem("ten");
     //     "11".innerHTML = localStorage.getItem("eleven");
     //     "12".innerHTML = localStorage.getItem("twelve");
-    //     "1".innerHTML = localStorage.getItem("one");
-    //     "2".innerHTML = localStorage.getItem("two");
-    //     "3".innerHTML = localStorage.getItem("three");
-    //     "4".innerHTML = localStorage.getItem("four");
-    //     "5".innerHTML = localStorage.getItem("five");
+    //     "13".innerHTML = localStorage.getItem("thirteen");
+    //     "14".innerHTML = localStorage.getItem("fourteen");
+    //     "15".innerHTML = localStorage.getItem("fifteen");
+    //     "16".innerHTML = localStorage.getItem("sixteen");
+    //     "17".innerHTML = localStorage.getItem("seventeen");
     //     }
 
-    // Need a save button
+    // Need a save button------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 });
