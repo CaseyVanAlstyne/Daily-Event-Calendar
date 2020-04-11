2624d42f0800662bfeb5013d23df999b5a6dc3da
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-04-11 09:50:16 -0400

    finished local storage and clear storage functions. Homework is done.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     var currentDay = $("#currentDay");
     var time = moment().format("H");
     var allTimeSlots = $(".content");
@@ -11,11 +10,9 @@ $(document).ready(function () {
     function updateTime() {
         currentDay.text(moment().format('dddd, MMMM Do YYYY, H'))
         console.log(time);
-    }
-    setInterval(updateTime, 1000);
+    } setInterval(updateTime, 1000);
 
     // use function to compare parsedTime to parsedLoop-----------------------------------------------------------------------------------------------
-
     allTimeSlots.each(function () {
         // console.log($(this).attr("id"));
         var loopId = $(this).attr("id")
@@ -33,40 +30,34 @@ $(document).ready(function () {
 
     // localstorage-- setItem ---------------------------------------------------------------------------------------------------------
     saveBtn.on('click', writeLocalStorage);
-
-
-
     function writeLocalStorage() {
         allTimeSlots.each(function () {
             // console.log(this);
             console.log($(this).val(), $(this).attr("id"));
             // $(this).val();
-            // if (typeof (Storage) !== "undefined") {
             localStorage.setItem($(this).attr("id"), $(this).val());
             // console.log("here here");
         });
     }
-
-
     // localStorage getItem ---------------------------------------------------------------------------------------------------------
-
     function updateContent() {
         $("#8").val(localStorage.getItem("8"));
-        // "9".innerHTML = localStorage.getItem("9");
-        // "10".innerHTML = localStorage.getItem("ten");
-        // "11".innerHTML = localStorage.getItem("eleven");
-        // "12".innerHTML = localStorage.getItem("twelve");
-        // "13".innerHTML = localStorage.getItem("thirteen");
-        // "14".innerHTML = localStorage.getItem("fourteen");
-        // "15".innerHTML = localStorage.getItem("fifteen");
-        // "16".innerHTML = localStorage.getItem("sixteen");
-        // "17".innerHTML = localStorage.getItem("seventeen");
+        console.log(localStorage.getItem("8"))
+        $("#9").val(localStorage.getItem("9"));
+        $("#10").val(localStorage.getItem("10"));
+        $("#11").val(localStorage.getItem("11"));
+        $("#12").val(localStorage.getItem("12"));
+        $("#13").val(localStorage.getItem("13"));
+        $("#14").val(localStorage.getItem("14"));
+        $("#15").val(localStorage.getItem("15"));
+        $("#16").val(localStorage.getItem("16"));
+        $("#17").val(localStorage.getItem("17"));
     }
-
     // clear localStorage---------------------------------------------------------------------------------------------------------
-
-    localStorage.clear();
-    clearBtn.on('click', writeLocalStorage);
+    clearBtn.on('click', function () {
+        localStorage.clear();
+        updateContent()
+    });
 
     updateContent();
 });
