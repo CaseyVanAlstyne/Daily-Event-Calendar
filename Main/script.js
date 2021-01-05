7c9586350fc08a4d3bb579510e51d2e119777ec8
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2021-01-04 19:45:11 -0500

    updated JS with new ID and Class references. Also corrected an error with the local storage.

--- Main/script.js
+++ Main/script.js
@@ -1,63 +1,52 @@
 $(document).ready(function () {
-    var currentDay = $("#currentDay");
-    var time = moment().format("H");
-    var allTimeSlots = $(".content");
-    var saveBtn = $("#saveBtn");
-    var clearBtn = $("#clearBtn");
-    // console.log(allTimeSlots);
+  // listen for save button clicks
+  $(".saveBtn").on("click", function () {
+    // get nearby values
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-    // display the current date and time in the currentDay Id on the HTML document------------------------------------------------------------------
-    function updateTime() {
-        currentDay.text(moment().format('dddd, MMMM Do YYYY, H'))
-        console.log(time);
-    } setInterval(updateTime, 1000);
+    // save in localStorage
+    localStorage.setItem(time, value);
+  });
 
-    // use function to compare parsedTime to parsedLoop-----------------------------------------------------------------------------------------------
-    allTimeSlots.each(function () {
-        // console.log($(this).attr("id"));
-        var loopId = $(this).attr("id")
-        var parsedTime = parseInt(time);
-        var parsedLoop = parseInt(loopId);
-        // console.log({ time, loopId, parsedLoop });
-        if (parsedTime < parsedLoop) {
-            $(this).addClass("future");
-        } else if (parsedTime > parsedLoop) {
-            $(this).addClass("past");
-        } else {
-            $(this).addClass("present");
-        }
+  function hourUpdater() {
+    // get current number of hours
+    var currentHour = moment().hours();
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
     });
+  }
 
-    // localstorage-- setItem ---------------------------------------------------------------------------------------------------------
-    saveBtn.on('click', writeLocalStorage);
-    function writeLocalStorage() {
-        allTimeSlots.each(function () {
-            // console.log(this);
-            console.log($(this).val(), $(this).attr("id"));
-            // $(this).val();
-            localStorage.setItem($(this).attr("id"), $(this).val());
-            // console.log("here here");
-        });
-    }
-    // localStorage getItem ---------------------------------------------------------------------------------------------------------
-    function updateContent() {
-        $("#8").val(localStorage.getItem("8"));
-        console.log(localStorage.getItem("8"))
-        $("#9").val(localStorage.getItem("9"));
-        $("#10").val(localStorage.getItem("10"));
-        $("#11").val(localStorage.getItem("11"));
-        $("#12").val(localStorage.getItem("12"));
-        $("#13").val(localStorage.getItem("13"));
-        $("#14").val(localStorage.getItem("14"));
-        $("#15").val(localStorage.getItem("15"));
-        $("#16").val(localStorage.getItem("16"));
-        $("#17").val(localStorage.getItem("17"));
-    }
-    // clear localStorage---------------------------------------------------------------------------------------------------------
-    clearBtn.on('click', function () {
-        localStorage.clear();
-        updateContent()
-    });
+  hourUpdater();
 
-    updateContent();
+  // set up interval to check if current time needs to be updated
+  var interval = setInterval(hourUpdater, 15000);
+
+  // load any saved data from localStorage
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  // display current day on page
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
